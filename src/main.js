09884f1b7c2acea6dcd1a6a8b5514d7a9c24e079
Muhammad Hilmi Al Fatih <hilmialf@gmail.com>
2020-12-02 18:10:28 +0900

    initialize vue vuex vue-router vuetify v-currency-field axios

--- src/main.js
+++ src/main.js
@@ -2,11 +2,31 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import vuetify from "./plugins/vuetify";
+import VCurrencyField from "v-currency-field";
+import { VTextField } from "vuetify/lib"; //Globally import VTextField
+
+Vue.component("v-text-field", VTextField);
+Vue.use(VCurrencyField, {
+  locale: "pt-BR",
+  decimalLength: 2,
+  autoDecimalMode: true,
+  min: null,
+  max: null,
+  defaultValue: 0,
+  valueAsInteger: false,
+  allowNegative: true
+});
+
+Vue.use(VueAxios, axios);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
