cdce5c955544278e7d5de2814d45c2c0b62bc9a2
Kouichi Abe(Grune) <k-abe@grune.co.jp>
2021-01-14 17:28:06 +0900

    Add axios.defaults.xsrfHeaderName = 'X-CSRF-Token';

--- src/main.js
+++ src/main.js
@@ -21,13 +21,14 @@ Vue.use(VCurrencyField, {
   allowNegative: true
 });
 
+// Automatically append CSRF token.
+axios.defaults.xsrfHeaderName = 'X-CSRF-Token';
 axios.defaults.withCredentials = true;
 axios.defaults.crossDomain = true;
+
 axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
 
 // Append Auth information if use basic auth.
-// console.debug(axios.defaults.baseURL);
-// console.debug(process.env.VUE_APP_API_ENDPOINT_BASICAUTH_NAME);
 if( process.env.VUE_APP_API_ENDPOINT_BASICAUTH_NAME != null){
   let username = process.env.VUE_APP_API_ENDPOINT_BASICAUTH_NAME;
   let password = process.env.VUE_APP_API_ENDPOINT_BASICAUTH_PASS;
