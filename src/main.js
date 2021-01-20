403afd8423929f60956a646f84d49178d6b1eb94
Kouichi Abe(Grune) <k-abe@grune.co.jp>
2021-01-20 10:04:20 +0900

    fix miss paste

--- src/main.js
+++ src/main.js
@@ -8,13 +8,6 @@ import vuetify from "./plugins/vuetify";
 import "@/plugins/axios.js";
 import "@/plugins/v-currency-field.js";
 
-axios.defaults.withCredentials = true;
-
-axios.defaults.crossDomain = true;
-
-
-axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT;
-
 /* About csrf token specification of Laravel6
   - X-CSRF-TOKEN: Specified token on html tag. It is priority than X-XSRF-TOKEN.
   - X-XSRF-TOKEN: Automatically set from cookie-value by axios. Laravel refer it if there is no X-CSRF-TOKEN.
