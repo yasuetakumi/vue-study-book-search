fdfcc795ac85d99c5d477363c81e2b01e90058cc
Kouichi Abe <k-abe@grune.co.jp>
2021-06-07 12:14:06 +0900

    Handshake error handling is updated

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -100,10 +100,12 @@ export default {
         {
           this.loading_hand_shake = false;
         }else{
-          throw new Error('hand shaking is not succeed. 1. If Request url is not correct,you can check client/.env file. 2.If file access is denied, please execute artisan config:clear in docker container, not on host. ');
+          throw new Error('hand shaking is not succeed.');
         }
       } catch (err) {
+        console.error('Hand shaking is not succeed. 1. If Request url is not correct,you can check client/.env file. 2.If file access is denied, please execute artisan config:clear in docker container, not on host. ');
         console.error(err);
+        
       }
     },
   },
