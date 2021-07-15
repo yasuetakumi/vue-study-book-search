7a3817f16ceed80b76d8731b84dfd6ef12911514
Kouichi Abe <k-abe@grune.co.jp>
2021-07-15 15:33:31 +0900

    Split backbutton to new Module.

---
+++ src/views/_components/GBackButton.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+    <v-container class="ml-6 px-1 my-0 py-0">
+        <v-row>
+            <v-col cols="12" lg="1">
+            <v-btn :to="targetRoute" block text plain class="">
+                <v-icon left>
+                mdi-keyboard-backspace
+                </v-icon>
+                {{ $t('general.action.backToList') }}
+            </v-btn>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+  props: {
+    targetRoute: {
+      type: Object,
+      default: () => ({}),
+    }
+  },
+};
+</script>
