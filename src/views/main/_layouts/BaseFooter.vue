95147cb9ad99bbb76c41a5a30149e12d993484f8
junpra <junia@gruneasia.info>
2021-12-20 12:27:36 +0800

    Add text and links to the footer - modify display if on mobile - change size icon

--- src/views/main/_layouts/BaseFooter.vue
+++ src/views/main/_layouts/BaseFooter.vue
@@ -2,13 +2,22 @@
   <v-footer class="footer" fixed>
     <v-col class="text-center" cols="12">
       <strong>Copyright {{ new Date().getFullYear() }} Grune.</strong> All rights reserved.
-      <span class="float-none float-sm-right">
+      <span class="float-none float-sm-right d-none d-sm-inline">
         制作会社: 
         <a href="https://grune.co.jp" target="_blank" style="color: #99c95a;text-decoration: underline;">
           株式会社Grune 
-          <v-icon color="#99c95a">mdi-launch</v-icon>
+          <v-icon size="15" color="#99c95a">mdi-launch</v-icon>
         </a>
       </span>
+      <div class="row d-block d-sm-none">
+        <v-col class="d-flex justify-center align-center">
+          制作会社: 
+          <a href="https://grune.co.jp" target="_blank" style="color: #99c95a;text-decoration: underline;">
+            株式会社Grune 
+            <v-icon size="15" color="#99c95a">mdi-launch</v-icon>
+          </a>
+        </v-col>
+      </div>
     </v-col>
   </v-footer>
 </template>
